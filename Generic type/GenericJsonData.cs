fc2229356e83d014eeb07c586c7a1b21cd87f532
Jamoliddin <shermatovjamoliddin3@gmail.com>
2022-10-16 03:04:15 +0500

    Add project files.

---
+++ Generic type/GenericJsonData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_type
+{
+    interface IGenericJsonData<T> where T : class
+    {
+       public void Save(T data);
+     
+
+        T Read();
+    }
+
+    class IGenericinterfaceJsonData<T> : IGenericJsonData<T> where T : class
+    {
+       public void Save(T user)
+        {
+            
+        }
+    public T Read()
+        {
+            return default;
+        }
+
+    }
+
+     class GenerateInterfaceData<InterfaceType, User> : IGenericinterfaceJsonData<User> where User : class 
+    {
+        public void Save()
+        {
+
+        }
+
+        public Type Read()
+        {
+            return default;
+        }
+    }
+}
---
+++ Generic type/Program.cs
@@ -0,0 +1,36 @@
+﻿using Generic_type;
+using Umbraco.Core.Models.Entities;
+
+public class Generic
+{
+    public static void Main(string[] args)
+    {
+        Generic<User> user = new Generic<User>();
+        user.ReadUser();
+        user.Save(new User());
+    }
+    static T SaveData<T, TData>() where T : class where TData : struct
+    {
+
+        return default;
+    }
+
+    static T result<T>()
+    {
+        return default;
+    }
+}
+
+public class Generic<Type> where Type : BaseClass, IEntity
+{
+    public void Save(Type data)
+    {
+
+    }
+
+    public Type ReadUser()
+    {
+        return default;
+    }
+
+}
